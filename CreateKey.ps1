8c9f241e5aa60f3f703797b7862ec57fb6f58e9c
Sameer <3660733+sameer-kumar@users.noreply.github.com>
2020-04-24 18:48:30 -0400

    Add files via upload

---
+++ CreateKey.ps1
@@ -0,0 +1,5 @@
+﻿$key = New-Object byte[] 32
+$generator = [System.Security.Cryptography.RandomNumberGenerator]::Create()
+$generator.GetBytes($key)
+$apiKey = [Convert]::ToBase64String($key)
+$generator.Dispose()
